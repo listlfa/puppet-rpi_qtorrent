b9067c6fb78c5c8b6992307d211d20469f431615
listlfa <list_lfa@yahoo.com>
2015-02-08 18:08:36 +1100

    Update setup.pp

--- setup.pp
+++ setup.pp
@@ -4,7 +4,19 @@ class qtorrent {
 	package { "tightvncserver":
 		ensure  => latest,
 	}
-
+	
+	# copied from
+	# http://elinux.org/RPi_VNC_Server#Run_at_boot
+	file { '/etc/init.d/vncboot':
+		ensure => file,
+		mode   => 755,
+		source => '/home/pi/github-listlfa/rpi_qtorrent/files/vncserver',
+    }
+    ->
+    exec { 'init.d--vncboot':
+      command => 'update-rc.d /etc/init.d/vncboot defaults',
+      path    => [ "/usr/sbin/", "/bin/" ],  # alternative syntax
+    }
 }
 
 include qtorrent
