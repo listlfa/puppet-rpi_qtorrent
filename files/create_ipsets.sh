07b5ebfe67d58544958c6b82d07ece37739482ca
listlfa <list_lfa@yahoo.com>
2016-05-22 22:26:45 +0800

    Update create_ipsets.sh
    add manual commands

--- files/create_ipsets.sh
+++ files/create_ipsets.sh
@@ -1,8 +1,10 @@
 #from http://dustinhatch.tumblr.com/post/33821945832/using-peerblock-lists-on-linux
 #https://www.centos.org/forums/viewtopic.php?t=8268
 
+# manual == sudo ipset create LEVEL1 hash:net maxelem 1048576
 ipset create LEVEL1 hash:net maxelem 1048576 
 
+# manual == curl -L "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=gz" | gunzip | cut -d: -f2 | grep -E "^[-0-9.]+$" | gawk '{print "add LEVEL1 "$1}' | ipset restore -exist
 curl -L "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=gz" |
     gunzip |
     cut -d: -f2 |
